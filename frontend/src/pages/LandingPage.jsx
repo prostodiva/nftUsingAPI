43ad20924d951217a2db34bed61cdf0cc653eea7
prostodiva <rkattsyna@mail.ru>
2025-04-04 14:06:34 -0700

    setup the dev environment. ready to work on the frontend

---
+++ frontend/src/pages/LandingPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const LandingPage = () => {
+    return (
+        <div className="min-h-screen flex flex-col">
+            <main className="flex-grow">
+                <div>
+                    <h1>NFT Marketplace test landing page</h1>
+                </div>
+            </main>
+        </div>
+    );
+};
+
+export default LandingPage; 
---
+++ frontend/vite.config.js
@@ -0,0 +1,26 @@
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    host: true,
+    port: 3000,
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    manifest: true,
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+    },
+  },
+})
